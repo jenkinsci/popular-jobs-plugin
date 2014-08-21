2dc70820fdd70aee44bebf72a27e2c92fcd1dbfe
robert.sandell <robert.sandell@sonymobile.com>
2014-08-21 11:58:03 +0200

    The first counter

---
+++ src/main/java/org/jenkinsci/plugins/popular_jobs/providers/JobVisitScore.java
@@ -0,0 +1,25 @@
+package org.jenkinsci.plugins.popular_jobs.providers;
+
+import hudson.model.Job;
+import org.jenkinsci.plugins.popular_jobs.Messages;
+import org.jenkinsci.plugins.popular_jobs.PopularExtension;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 23052130
+ * Date: 2014-08-21
+ * Time: 11:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobVisitScore extends PopularExtension {
+    @Override
+    public int getScore(Job job, StaplerRequest request) {
+        return 1;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return Messages.JobVisitScore_DisplayName();
+    }
+}
