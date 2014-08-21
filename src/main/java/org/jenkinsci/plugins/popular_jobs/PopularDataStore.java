4b3978f515951ad99866e2c01b73f70826fc457c
robert.sandell <robert.sandell@sonymobile.com>
2014-08-21 12:06:26 +0200

    Simple in memory persistence

--- src/main/java/org/jenkinsci/plugins/popular_jobs/PopularDataStore.java
+++ src/main/java/org/jenkinsci/plugins/popular_jobs/PopularDataStore.java
@@ -2,6 +2,9 @@ package org.jenkinsci.plugins.popular_jobs;
 
 import hudson.model.Job;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: 23052130
@@ -10,7 +13,50 @@ import hudson.model.Job;
  * To change this template use File | Settings | File Templates.
  */
 public class PopularDataStore {
+    //temporary measure
+    private static Map<Job, Map<PopularExtension, Score>> data = new HashMap<Job, Map<PopularExtension, Score>>();
+    
     public static void store(Job job, int score, PopularExtension provider) {
+        Map<PopularExtension, Score> map = data.get(job);
+        if (map == null) {
+            map = new HashMap<PopularExtension, Score>();
+            data.put(job, map);
+        }
+        map.put(provider, new Score(job, score, provider));
+    }
 
+    public static int getScore(Job j) {
+        int s = 0;
+        Map<PopularExtension, Score> map = data.get(j);
+        if (map != null) {
+            for(Score d: map.values()) {
+                s += d.getScore();
+            }
+        }
+        return s;
+    }
+    
+    public static class Score {
+        private Job job; 
+        private int score; 
+        private PopularExtension provider;
+
+        public Score(Job job, int score, PopularExtension provider) {
+            this.job = job;
+            this.score = score;
+            this.provider = provider;
+        }
+
+        public Job getJob() {
+            return job;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public PopularExtension getProvider() {
+            return provider;
+        }
     }
 }
